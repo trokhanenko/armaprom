7157b897550cf51906d7cf8eba8446f786e596ef
Ivan_Trokhanenko <gamernext@gmail.com>
2017-06-23 15:51:31 +0300

    Good site)

---
+++ themes/arma/js/custom.js
@@ -0,0 +1,32 @@
+(function ($, Drupal, window, document) {
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context, settings) {
+      // Behavior вызывается несколько раз на странице, не забывайте использовать функцию .once().
+      $('.view-slaider .view-content').once().slick({
+      	 autoplay: true,
+  		 autoplaySpeed: 4000,
+      	 dots: true,
+      	 arrows: false,
+      	 adaptiveHeight: true
+      });
+
+      if (window.matchMedia('screen and (max-width: 767px)').matches) {
+        $('.menu').once().addClass('collapse');
+      }
+      $("#block-produkcia-menu").once().click(function() {
+        if (window.matchMedia('screen and (max-width: 767px)').matches) {
+          $('.menu').collapse('toggle');
+        }
+      });
+    }
+  };
+})(jQuery, Drupal, this, this.document);
+
+(function ($, Drupal, window, document) {
+  Drupal.behaviors.accordionBehavior = {
+    attach: function (context, settings) {
+      // Behavior вызывается несколько раз на странице, не забывайте использовать функцию .once().
+      
+    }
+  };
+})(jQuery, Drupal, this, this.document);
