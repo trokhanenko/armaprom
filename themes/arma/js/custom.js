522d7e696eacc58fef75733f47322dd8213172b8
Ivan_Trokhanenko <gamernext@gmail.com>
2017-06-30 09:54:15 +0300

    Update sitebar first for mobile

--- themes/arma/js/custom.js
+++ themes/arma/js/custom.js
@@ -3,19 +3,19 @@
     attach: function (context, settings) {
       // Behavior вызывается несколько раз на странице, не забывайте использовать функцию .once().
       $('.view-slaider .view-content').once().slick({
-      	 autoplay: true,
-  		 autoplaySpeed: 4000,
-      	 dots: true,
-      	 arrows: false,
-      	 adaptiveHeight: true
+      	autoplay: true,
+  		  autoplaySpeed: 4000,
+      	dots: true,
+      	arrows: false,
+      	adaptiveHeight: true
       });
 
       if (window.matchMedia('screen and (max-width: 767px)').matches) {
-        $('.menu').once().addClass('collapse');
+        $('#block-produkcia .menu').once().addClass('collapse');
       }
       $("#block-produkcia-menu").once().click(function() {
         if (window.matchMedia('screen and (max-width: 767px)').matches) {
-          $('.menu').collapse('toggle');
+          $('#block-produkcia .menu').collapse('toggle');
         }
       });
     }
